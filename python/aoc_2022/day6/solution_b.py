bdd8818e75307efa8f1f38fc7fbe2d13cbc03df4
Jelmer Kuperus <jelmer@miro.com>
2022-12-06 09:23:28 +0100

    python

---
+++ python/aoc_2022/day6/solution_b.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    
+    with open("inputs/day6/input_a.txt", "r") as f:
+        input = [*f.read()]
+        
+    window_size = 14
+    
+    windows = map(lambda i: input[i: i + window_size], range(len(input) - window_size + 1))
+    index, window = next(filter(lambda pair: len(pair[1]) == len(set(pair[1])), enumerate(windows)))
+    
+    result = index + window_size
+    print(result)
+    
